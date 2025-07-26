2ace7118513702ac3decd3ba7afed9c9189283ef
L1F3atbhss <Nathan.chan3000@gmail.com>
2025-07-26 09:57:35 -0400

    Update agent.py

--- src/voice_agent/agent.py
+++ src/voice_agent/agent.py
@@ -1,7 +1,7 @@
 from google.adk.agents import Agent
 from google.adk.tools import google_search  # Import the tool
 
-food = "remen"
+food = "pizza dough, tomato sauce, mozzarella cheese, pepperoni, basil"
 
 root_agent = Agent(
    # A unique name for the agent.
@@ -15,7 +15,7 @@ root_agent = Agent(
    description="Agent to give only" + food + "resipes",
 
    # instruction="You are an expert researcher. Only give short, factual answers — 2-3 sentences max.",
-   instruction="give only" + food + "resipes",
+   instruction="give only" + food + "resipes and talk slowly. End with should I continue?",
    # Add google_search tool to perform grounding with Google search.
    tools=[google_search]
    )
