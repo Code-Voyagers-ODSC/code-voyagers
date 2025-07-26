4cccaec58a183ba6637097fbb0c3ccf92dd0d1b8
L1F3atbhss <Nathan.chan3000@gmail.com>
2025-07-26 01:21:40 -0400

    mm ramen

--- src/voice_agent/agent.py
+++ src/voice_agent/agent.py
@@ -2,7 +2,6 @@ from google.adk.agents import Agent
 from google.adk.tools import google_search  # Import the tool
 
 food = "remen"
-promnt = "give only" + food + "resipes"
 
 root_agent = Agent(
    # A unique name for the agent.
@@ -13,10 +12,10 @@ root_agent = Agent(
    model="gemini-live-2.5-flash-preview",  # for example: model="gemini-2.0-flash-live-001" or model="gemini-2.0-flash-live-preview-04-09"
    # A short description of the agent's purpose.
    #description="Agent to answer questions using Google Search.",
-   description="Agent to give out cookie resips",
+   description="Agent to give only" + food + "resipes",
 
    # instruction="You are an expert researcher. Only give short, factual answers — 2-3 sentences max.",
-   instruction="give only cookie resipes",
+   instruction="give only" + food + "resipes",
    # Add google_search tool to perform grounding with Google search.
    tools=[google_search]
    )
