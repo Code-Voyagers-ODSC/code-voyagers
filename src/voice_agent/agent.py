6f1b7db95bead0e3862e76a929acc13b09231d60
L1F3atbhss <Nathan.chan3000@gmail.com>
2025-07-26 01:12:38 -0400

    cookies

--- src/voice_agent/agent.py
+++ src/voice_agent/agent.py
@@ -1,6 +1,9 @@
 from google.adk.agents import Agent
 from google.adk.tools import google_search  # Import the tool
 
+food = "remen"
+promnt = "give only" + food + "resipes"
+
 root_agent = Agent(
    # A unique name for the agent.
    name="basic_search_agent",
@@ -9,9 +12,11 @@ root_agent = Agent(
    # https://google.github.io/adk-docs/get-started/streaming/quickstart-streaming/#supported-models
    model="gemini-live-2.5-flash-preview",  # for example: model="gemini-2.0-flash-live-001" or model="gemini-2.0-flash-live-preview-04-09"
    # A short description of the agent's purpose.
-   description="Agent to answer questions using Google Search.",
-   # Instructions to set the agent's behavior.
-   instruction="You are an expert researcher. You always stick to the facts.",
+   #description="Agent to answer questions using Google Search.",
+   description="Agent to give out cookie resips",
+
+   # instruction="You are an expert researcher. Only give short, factual answers — 2-3 sentences max.",
+   instruction="give only cookie resipes",
    # Add google_search tool to perform grounding with Google search.
    tools=[google_search]
-)
+   )
